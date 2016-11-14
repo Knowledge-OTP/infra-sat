298e7d865c6116208d27a27d5e5dbce73872c736
Oded <oded.sharir@gmail.com>
2016-11-14 17:04:06 +0200

    new dist

--- dist/main.js
+++ dist/main.js
@@ -1526,6 +1526,7 @@ angular.module('znk.infra-sat.completeExerciseSat').run(['$templateCache', funct
     "        <performance-timeline\n" +
     "            on-timeline-finish=\"$ctrl.onTimelineFinish(subjectDelta)\"\n" +
     "            subject-id=\"{{::$ctrl.exerciseContent.subjectId}}\"\n" +
+    "            show-induction=\"true\"\n" +
     "            active-exercise-id=\"::$ctrl.exerciseContent.id\">\n" +
     "        </performance-timeline>\n" +
     "    </section>\n" +
@@ -2535,7 +2536,8 @@ angular.module('znk.infra-sat.exerciseUtilitySat').run(['$templateCache', functi
                 var directive = {
                     scope: {
                         onTimelineFinish: '&',
-                        activeExerciseId: '=?'
+                        activeExerciseId: '=?',
+                        showInduction: '<?'
                     },
                     restrict: 'E',
                     templateUrl: 'components/performance/directives/performanceTimeline/performanceTimeline.template.html',
@@ -2971,7 +2973,7 @@ angular.module('znk.infra-sat.performance').run(['$templateCache', function($tem
     "        <div class=\"progress-val\" ng-style=\"vm.timelineMinMaxStyle\" ng-if=\"vm.timeLineData.data.length\">\n" +
     "            <div class=\"goal-wrapper\">{{vm.goalPerSubject}}\n" +
     "                <div class=\"timeline-plus\"\n" +
-    "                     ng-if=\"vm.timelineLinePlus\"\n" +
+    "                     ng-if=\"vm.timelineLinePlus && vm.showInduction\"\n" +
     "                     ng-class=\"{ 'red-point': vm.isRed, 'green-point': !vm.isRed }\">\n" +
     "                    {{vm.timelineLinePlus}}\n" +
     "                </div>\n" +
