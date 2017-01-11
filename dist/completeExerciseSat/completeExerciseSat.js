46c86a29cb2d61072df09ad0f582bfcbb20ceb40
orenor10 <orenor10@barak.net.il>
2017-01-11 16:02:50 +0200

    sat new logo

--- dist/completeExerciseSat/completeExerciseSat.js
+++ dist/completeExerciseSat/completeExerciseSat.js
@@ -39,7 +39,8 @@
 
             var svgMap = {
                 'complete-exercise-correct-icon': 'components/completeExerciseSat/svg/correct-icon.svg',
-                'complete-exercise-wrong-icon': 'components/completeExerciseSat/svg/wrong-icon.svg'
+                'complete-exercise-wrong-icon': 'components/completeExerciseSat/svg/wrong-icon.svg',
+                'znk-app-name-logo': 'components/configSat/svg/znk-app-name-logo.svg'
             };
             SvgIconSrvProvider.registerSvgSources(svgMap);
         }]);
